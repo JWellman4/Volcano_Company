8fa7a1cf54043ea22f38110c55cf2c2d655d7f28
JWellman4 <52516150+JWellman4@users.noreply.github.com>
2024-04-15 09:10:18 -0400

    Update 5_Profit.py

--- pages/5_Profit.py
+++ pages/5_Profit.py
@@ -29,6 +29,13 @@ def options_select5():
         else:
             st.session_state["max_selections5"] = len(available_options5)
 
+def date_selected5():
+    if "selected_date5" not in st.session_state:
+        if len(select_week) > 1:
+            end_date = pd.to_datetime(select_week[1])
+        else:
+            end_date = start_date
+
 def format_sales(sales_card):
     if abs(sales_card) >= 1_000_000:
         return '${:.1f}M'.format(sales_card / 1_000_000)
@@ -60,6 +67,8 @@ with st.sidebar:
     select_week = st.date_input(
         label='Select Date(s)',
         value=(min_date, max_date),
+        key="selected_date5",
+        on_change=date_selected5
     )
     select_options5 =st.multiselect(
             label="Select an Option",
